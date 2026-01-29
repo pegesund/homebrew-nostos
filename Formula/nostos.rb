9973f7de95f5fddf262cc466ef332a57a81fa8c7
Petter <petter.egesund@gmail.com>
2026-01-29 14:30:40 +0100

    Update formula for v0.2.0 with correct checksums

--- Formula/nostos.rb
+++ Formula/nostos.rb
@@ -1,35 +1,29 @@
 class Nostos < Formula
   desc "Functional programming language with lightweight processes and non-blocking I/O"
   homepage "https://github.com/pegesund/nostos"
-  version "0.1.0"
+  version "0.2.0"
   license "MIT"
 
-  # TODO: Update these URLs and checksums when you create a release
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/pegesund/nostos/releases/download/v#{version}/nostos-#{version}-aarch64-apple-darwin.tar.gz"
-      sha256 "REPLACE_WITH_ARM64_SHA256"
+      url "https://github.com/pegesund/nostos/releases/download/v#{version}/nostos-v#{version}-aarch64-apple-darwin.tar.gz"
+      sha256 "8771f55be42338c68a64e7f350caf74596ce7716e27a393d5c1e71c96d4e9c60"
     else
-      url "https://github.com/pegesund/nostos/releases/download/v#{version}/nostos-#{version}-x86_64-apple-darwin.tar.gz"
-      sha256 "REPLACE_WITH_X86_64_SHA256"
+      url "https://github.com/pegesund/nostos/releases/download/v#{version}/nostos-v#{version}-x86_64-apple-darwin.tar.gz"
+      sha256 "5a3652e1942f8891dd6cd111728c9091a670c7e9fc631bc2c13c6323f660b831"
     end
   end
 
   on_linux do
-    if Hardware::CPU.arm?
-      url "https://github.com/pegesund/nostos/releases/download/v#{version}/nostos-#{version}-aarch64-unknown-linux-gnu.tar.gz"
-      sha256 "REPLACE_WITH_LINUX_ARM64_SHA256"
-    else
-      url "https://github.com/pegesund/nostos/releases/download/v#{version}/nostos-#{version}-x86_64-unknown-linux-gnu.tar.gz"
-      sha256 "REPLACE_WITH_LINUX_X86_64_SHA256"
-    end
+    url "https://github.com/pegesund/nostos/releases/download/v#{version}/nostos-v#{version}-x86_64-unknown-linux-gnu.tar.gz"
+    sha256 "5979eca043cc831e74ef91e3970ccbc5a42aa6e530639ede6078c2577784d304"
   end
 
   def install
     bin.install "nostos"
     bin.install "nostos-lsp" if File.exist?("nostos-lsp")
-    
-    # Install stdlib
+
+    # Install stdlib (also embedded in binary, but useful for reference)
     (share/"nostos"/"stdlib").install Dir["stdlib/*"] if File.directory?("stdlib")
   end
 
